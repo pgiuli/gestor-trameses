6cbba4c90db9e3e54a5cee65e749537cd5bb3537
Pau Giuli <pau@giuli.cat>
2023-12-24 17:27:29 +0100

    v0.2.2

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, send_file, redirect
 import db
 import dbexport
+
 
 flask_app = Flask(__name__)
 
@@ -13,10 +14,11 @@ def enviament():
         code = request.form.get('code')
         password = request.form.get('password')
         save_status = db.save_submission(user, password, task, code)
+        print(save_status)
         return f'''
             <link rel="stylesheet" type="text/css" href="/static/styles.css">
             <h1>Estat de la tramesa: {save_status}</h1>
-            <a href="/">Torna a l'inici.</a>
+            <a href="/">Torna</a>
             '''
     else:
         print('Showing Form')
@@ -51,10 +53,35 @@ def index():
             </body>
         </html>
     """
+
+@flask_app.route('/descarrega')
+def download():
+    #Create submissions .zip file
+    dbexport.export_submissions()
+    #Return submissions .zip file and redirect to index
+    return send_file('submissions.zip', as_attachment=True)
+
+
+#Make any 500 error show contact info
+@flask_app.errorhandler(500)
+def server_error(e):
+    return """
+        <html>
+            <head>
+                <link rel="stylesheet" type="text/css" href="/static/styles.css">
+            </head>
+            <body>
+                <h1>500 Error</h1>
+                <h3>Hi ha hagut un error al servidor. Contacta amb l'administrador.</h3>
+                <p href=https://giuli.cat>Creat per: Pau Giuli</p>
+            </body>
+        </html>
+    """
+
 #Make any 404 error redirect to the index page
 @flask_app.errorhandler(404)
 def page_not_found(e):
-    return index()
+    return redirect('/')
 
 
 if __name__ == '__main__':
